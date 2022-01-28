901c787cfd0945bf108adb5ae4b0a4d7a5671205
varuncy96 <varuncy@gmail.com>
2022-01-28 16:21:17 +0530

    Initial commit

---
+++ src/test/java/day1/JavaBasics.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class JavaBasics {
+    public static void main(String[] args) {
+        System.out.println("print age");
+        int a=10;
+
+        System.out.println(a);
+        a=20;
+        System.out.println(a);
+
+    }
+}
